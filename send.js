7b52a83dfafad2de77c5b51b9e6f65ddb457908a
Dice A <51027869+DiceRandom@users.noreply.github.com>
2020-03-26 18:26:37 -0400

    Update send.js

--- send.js
+++ send.js
@@ -3,7 +3,7 @@ var uid = "i32v9nlxq9vi9rtbdhs5c5abn";
 var access_token = "A";
 var url = 'https://api.spotify.com/v1/users/{uid}/playlists'
 
-create_playlist();
+create_playlist(); 
 
 function create_playlist(){
 
